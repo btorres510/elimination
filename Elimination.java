13fe45f0d89e824dc5fc9713534a475a3987d38c
btorres510 <byron.torres510@gmail.com>
2017-05-17 01:28:49 -0500

    Update Elimination.java

--- Elimination.java
+++ Elimination.java
@@ -7,6 +7,7 @@
  * @since   2017-05-16
  */
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Elimination {
@@ -14,11 +15,32 @@ public class Elimination {
         Board board = new Board();
         PairOfDice dice = new PairOfDice();
         Scanner scan = new Scanner(System.in);
+        boolean endGame = false;
         String userInput;
-        System.out.print("Welcome to Elimination!" +
+        System.out.println("Welcome to Elimination!" +
                 "\n The dice will be rolled," +
                 "\n And the game will proceed from there. \n");
-        dice.roll();
-        System.out.print("\n Dice 1: " + dice.getDie1() + " \n Dice 2: " + dice.getDie2());
+        while(!endGame) {
+            dice.roll();
+            System.out.println("Dice 1: " + dice.getDie1());
+            System.out.print("Dice 2: " + dice.getDie2());
+            endGame = board.endOfGame();
+            try {
+                System.out.print("\n Do you want to eliminate the sum or the two numbers?" +
+                        "\n Enter 's' for sum, 'b' for both numbers: ");
+                userInput = scan.next();
+                if (userInput.equalsIgnoreCase("s")) {
+                    board.sum(dice.getTotal());
+                } else if (userInput.equalsIgnoreCase("b")) {
+                    board.bothDie(dice.getDie1(), dice.getDie2());
+                } else {
+                    throw new InputMismatchException("Enter either 's' or 'b'");
+                }
+            } catch (InputMismatchException ime) {
+                ime.printStackTrace();
+            }
+            System.out.println("\n" + board);
+            System.out.println(board.getScore() + "\n");
+        }
     }
 }
